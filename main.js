60c8dd8ecf52a4da6cfe26c13b405c372ef30a5f
Cassio Pinheiro <cassiochan.u@gmail.com>
2023-06-04 21:34:43 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp(); 
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const EnderecoDaNovaImagem = $('#Endereco-imagem-nova').val();
+        const NovoItem = $('<li style="display:none"></li>');
+        $(`<img src = "${EnderecoDaNovaImagem}" />`).appendTo (NovoItem);
+        $(`
+           <div class = "overlay-imagem-link">
+                  <a href= "${EnderecoDaNovaImagem}" target = "_blank" title = "Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                  </a>
+           </div>
+        `).appendTo(NovoItem);
+        $(NovoItem).appendTo('ul');
+        $(NovoItem).fadeIn(1000);
+        $('#Endereco-imagem-nova').val('')
+    })
+})
